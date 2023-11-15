c07f4a30b66a047d5e9ee8d5c7f54846857bc85e
Yubin Shin <68121478+YubinShin@users.noreply.github.com>
2023-11-15 13:30:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 from API.routes.api_routes import api_router
 
 app = FastAPI()
@@ -30,6 +31,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    GZipMiddleware, minimum_size=1000
 )
 
 app.include_router(api_router, prefix="/api")
